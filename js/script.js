1a8268f6f96d3fbd9c2f8ca3aa96c890862ed913
douwevdruenen <90314186+douwevdruenen@users.noreply.github.com>
2024-01-23 19:59:10 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -43,11 +43,44 @@ if (localStorage.getItem("darkMode")) {
   document.body.classList.add("dark-mode");
 }
 
+// document
+//   .getElementById("dark-mode-toggle")
+//   .addEventListener("change", function () {
+//     var homeFoto = document.getElementById("home-foto");
+//     var isIndexPage = window.location.pathname.endsWith("index.html");
+
+//     if (this.checked) {
+//       document.body.classList.add("dark-mode");
+//       if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home-white.svg";
+//       localStorage.setItem("darkMode", true);
+//     } else {
+//       document.body.classList.remove("dark-mode");
+//       if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home.svg";
+//       localStorage.removeItem("darkMode");
+//     }
+//   });
+
+// if (localStorage.getItem("darkMode")) {
+//   document.getElementById("dark-mode-toggle").checked = true;
+//   document.body.classList.add("dark-mode");
+//   var homeFoto = document.getElementById("home-foto");
+//   var isIndexPage = window.location.pathname.endsWith("index.html");
+//   if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home-white.svg";
+// }
+
+var isIndexPage =
+  window.location.pathname.endsWith("index.html") ||
+  window.location.pathname.endsWith("/") ||
+  window.location.pathname === "";
+
 document
   .getElementById("dark-mode-toggle")
   .addEventListener("change", function () {
     var homeFoto = document.getElementById("home-foto");
-    var isIndexPage = window.location.pathname.endsWith("index.html");
+    var isIndexPage =
+      window.location.pathname.endsWith("index.html") ||
+      window.location.pathname.endsWith("/") ||
+      window.location.pathname === "";
 
     if (this.checked) {
       document.body.classList.add("dark-mode");
@@ -64,6 +97,9 @@ if (localStorage.getItem("darkMode")) {
   document.getElementById("dark-mode-toggle").checked = true;
   document.body.classList.add("dark-mode");
   var homeFoto = document.getElementById("home-foto");
-  var isIndexPage = window.location.pathname.endsWith("index.html");
+  var isIndexPage =
+    window.location.pathname.endsWith("index.html") ||
+    window.location.pathname.endsWith("/") ||
+    window.location.pathname === "";
   if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home-white.svg";
 }
