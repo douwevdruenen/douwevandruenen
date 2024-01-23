fe39e4f1e0cd0d626e538bd9b3d08b6295871c71
douwevdruenen <90314186+douwevdruenen@users.noreply.github.com>
2024-01-23 19:38:49 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,3 +38,32 @@ $(window).scroll(function () {
   }
   lastScrollTop = currentScrollTop;
 });
+
+if (localStorage.getItem("darkMode")) {
+  document.body.classList.add("dark-mode");
+}
+
+document
+  .getElementById("dark-mode-toggle")
+  .addEventListener("change", function () {
+    var homeFoto = document.getElementById("home-foto");
+    var isIndexPage = window.location.pathname.endsWith("index.html");
+
+    if (this.checked) {
+      document.body.classList.add("dark-mode");
+      if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home-white.svg";
+      localStorage.setItem("darkMode", true);
+    } else {
+      document.body.classList.remove("dark-mode");
+      if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home.svg";
+      localStorage.removeItem("darkMode");
+    }
+  });
+
+if (localStorage.getItem("darkMode")) {
+  document.getElementById("dark-mode-toggle").checked = true;
+  document.body.classList.add("dark-mode");
+  var homeFoto = document.getElementById("home-foto");
+  var isIndexPage = window.location.pathname.endsWith("index.html");
+  if (homeFoto && isIndexPage) homeFoto.src = "img/foto-home-white.svg";
+}
